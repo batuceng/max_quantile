8dbd257ba736a22f4c332987abe87acb51653ab7
batuceng <cengiz16@itu.edu.tr>
2024-09-16 23:46:40 +0300

    updaets

--- data/dataset.py
+++ data/dataset.py
@@ -22,3 +22,23 @@ class CustomDataset(Dataset):
     def load_data(self):
         X, Y = np.load(self.path)
         
+class UnconditionalSynthData(Dataset):
+    def __init__(self, X, Y, transform=None):
+        self.path = './raw/data1.npy'
+        self.X = X
+        self.Y = Y
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        x = self.X[idx]
+        y = self.Y[idx]
+        if self.transform:
+            x = self.transform(x)
+        return x, y
+    
+    def load_data(self):
+        X, Y = np.load(self.path)
+        
