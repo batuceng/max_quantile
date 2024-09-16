5c770b70af75480799ff045562e7547436c738c9
batuceng <cengiz16@itu.edu.tr>
2024-09-16 23:46:28 +0300

    added quantizers

---
+++ quantizers/quantizer.py
@@ -0,0 +1,15 @@
+import numpy as np
+import torch
+
+
+class GridQuantizer():
+    def __init__():
+        pass
+    
+class VoronoiQuantizer():
+    def __init__():
+        pass
+    
+class QuadTreeQuantizer():
+    def __init__():
+        pass
